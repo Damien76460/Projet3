55cceeadd749b1fb03e4334b6d19f6198a81dad3
ThomasClvr <99747179+ThomasClvr@users.noreply.github.com>
2022-03-31 17:34:58 +0200

    Add Class regles

---
+++ regle.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class Regles(object):
+
+    # Constructor
+    def __init__(self, level=1):
+        self._regle = regle
+
+    def get_regle(self):
+        return self._regle
